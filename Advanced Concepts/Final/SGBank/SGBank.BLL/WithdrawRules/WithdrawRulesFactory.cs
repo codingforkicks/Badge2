138ea0bdfcece7c91c39e151c4455500f31ea76c
Tia Simmons <t.simmons06@yahoo.com>
2020-09-26 13:48:50 -0400

    task 3 completed

---
+++ Advanced Concepts/Final/SGBank/SGBank.BLL/WithdrawRules/WithdrawRulesFactory.cs
@@ -0,0 +1,18 @@
+﻿using SGBank.Models;
+using SGBank.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGBank.BLL.WithdrawRules
+{
+    public class WithdrawRulesFactory
+    {
+        public static IWithdraw Create(AccountType account)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
