bc2dfaaf1fef8b4232744bbbb06735c91edab983
Tia Simmons <t.simmons06@yahoo.com>
2020-09-26 18:18:03 -0400

    integrate live data started

---
+++ Advanced Concepts/Final/SGBank/SGBank.Data/FileAccountRepository.cs
@@ -0,0 +1,26 @@
+﻿using SGBank.Models;
+using SGBank.Models.Responses;
+using SGBank.Models.Interfaces;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGBank.Data
+{
+    public class FileAccountRepository : IAccountRepository
+    {
+        //open repository file if it exists
+        string path = @"c:\Data\Accounts.txt";
+
+        if(File.Exists(path)){
+
+        } else {
+            File.Create()
+        }
+
+        
+    }
+}
