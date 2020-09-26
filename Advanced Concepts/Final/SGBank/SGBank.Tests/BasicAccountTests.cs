86d166a7c554ef682e91b12adc43a5c0e9bd7872
Tia Simmons <t.simmons06@yahoo.com>
2020-09-26 16:45:33 -0400

    task 7 complete: basic account withdraw test functional

--- Advanced Concepts/Final/SGBank/SGBank.Tests/BasicAccountTests.cs
+++ Advanced Concepts/Final/SGBank/SGBank.Tests/BasicAccountTests.cs
@@ -40,5 +40,27 @@ namespace SGBank.Tests
 
             Assert.AreEqual(expectedResult, response.Success);
         }
+
+        [TestCase("33333", "Basic Account", 1500, AccountType.Basic, -1000, 1500, false)]
+        [TestCase("33333", "Basic Account", 100, AccountType.Free, -100, 100, false)]
+        [TestCase("33333", "Basic Account", 100, AccountType.Basic, 100, 100, false)]
+        [TestCase("33333", "Basic Account", 150, AccountType.Basic, -50, 100, true)]
+        [TestCase("33333", "Basic Account", 100, AccountType.Basic, -150, -60, true)]
+        public void BasicAccountWithdrawRuleTest(string accountNumber, string name, decimal balance, AccountType accountType, decimal amount, decimal newBalance, bool expectedResult)
+        {
+            IWithdraw withdraw = new BasicAccountWithdrawRule();
+            Account testAccount = new Account
+            {
+                Name = name,
+                Balance = balance,
+                AccountNumber = accountNumber,
+                Type = accountType
+            };
+
+            AccountWithdrawResponse response = new AccountWithdrawResponse();
+            response = withdraw.Withdraw(testAccount, amount);
+
+            Assert.AreEqual(expectedResult, response.Success);
+        }
     }
 }
