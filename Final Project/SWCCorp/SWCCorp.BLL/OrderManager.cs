ac904044c729a562973645dd097bd6ba51e1ec2d
Tia Simmons <t.simmons06@yahoo.com>
2020-10-31 15:53:37 -0400

    order responses implimented

---
+++ Final Project/SWCCorp/SWCCorp.BLL/OrderManager.cs
@@ -0,0 +1,37 @@
+﻿using SWCCorp.Models.Interfaces;
+using SWCCorp.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWCCorp.BLL
+{
+    public class OrderManager
+    {
+        private IOrderRepository _orderRepository;
+
+        public OrderManager(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public OrderLookUpResponse LookupOrder(string date)
+        {
+            OrderLookUpResponse response = new OrderLookUpResponse();
+            response.Order = _orderRepository.DisplayOrders(date);
+
+            if(response.Order == null)
+            {
+                response.Success = false;
+                response.Message = $"No orders were found for {date}.";
+            } else
+            {
+                response.Success = true;
+            }
+
+            return response;
+        }
+    }
+}
