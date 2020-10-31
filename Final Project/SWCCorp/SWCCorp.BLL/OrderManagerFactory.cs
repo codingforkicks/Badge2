a4c0e699e98597f7bb73bb7d126dc0dcc4dc3d71
Tia Simmons <t.simmons06@yahoo.com>
2020-10-31 16:08:27 -0400

    order manager factory created

---
+++ Final Project/SWCCorp/SWCCorp.BLL/OrderManagerFactory.cs
@@ -0,0 +1,25 @@
+﻿using SWCCorp.Data;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWCCorp.BLL
+{
+    public static class OrderManagerFactory
+    {
+        public static OrderManager Create()
+        {
+            string mode = ConfigurationManager.AppSettings["Mode"].ToString();
+            switch (mode)
+            {
+                case "Test":
+                    return new OrderManager(new OrderTestRepository());
+                default:
+                    throw new Exception("Mode value in app config is not valid");
+            }
+        }
+    }
+}
---
+++ Final Project/SWCCorp/SWCCorp.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWCCorp.Tests
+{
+    public class Class1
+    {
+    }
+}
