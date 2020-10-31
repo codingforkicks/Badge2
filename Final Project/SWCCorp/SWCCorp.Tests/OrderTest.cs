f29e298d6feb44cd3e8f8207df0ed6dcdde2bc3e
Tia Simmons <t.simmons06@yahoo.com>
2020-10-31 16:24:39 -0400

    test data is loading successfully

---
+++ Final Project/SWCCorp/SWCCorp.Tests/OrderTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SWCCorp.BLL;
+using SWCCorp.Models.Responses;
+
+namespace SWCCorp.Tests
+{
+    [TestFixture]
+    public class OrderTest
+    {
+        [Test]
+        public void CanLoadTestData()
+        {
+            OrderManager manager = OrderManagerFactory.Create();
+
+            OrderLookUpResponse response = manager.LookupOrder("");
+
+            Assert.IsNotNull(response.Order);
+            Assert.IsTrue(response.Success);
+            Assert.AreEqual(1, response.Order.OrderNumber);
+        }
+    }
+}
